2dee09ee08ed7e78360fcdc6177a68cb920c4404
MaxBaton <maks.bay.2015@mail.ru>
2023-12-12 14:52:53 +0300

    add dispatcher to repository

--- feature-all-users/data/src/main/java/com/maxbay/data/repository/UserRepositoryImpl.kt
+++ feature-all-users/data/src/main/java/com/maxbay/data/repository/UserRepositoryImpl.kt
@@ -5,13 +5,20 @@ import com.maxbay.data.network.api.UserApi
 import com.maxbay.data.network.api.UserApiHelper
 import com.maxbay.domain.models.User
 import com.maxbay.domain.repository.UserRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 
 class UserRepositoryImpl(
-    private val userApiHelper: UserApiHelper
+    private val userApiHelper: UserApiHelper,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ): UserRepository {
     override fun observeUsers(): Flow<List<User>> {
-        return userApiHelper.getAllUsers().map { it.toDomain() }
+        return userApiHelper
+            .getAllUsers()
+            .flowOn(context = dispatcher)
+            .map { it.toDomain() }
     }
 }
