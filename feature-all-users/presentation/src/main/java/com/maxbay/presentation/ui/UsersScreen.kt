39173524652f07aee1ad18caceb166f5580e86d1
MaxBaton <maks.bay.2015@mail.ru>
2023-12-13 16:49:27 +0300

    add simple ui for users screen

--- feature-all-users/presentation/src/main/java/com/maxbay/presentation/ui/UsersScreen.kt
+++ feature-all-users/presentation/src/main/java/com/maxbay/presentation/ui/UsersScreen.kt
@@ -1,6 +1,8 @@
 package com.maxbay.presentation.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,7 +15,19 @@ fun UsersScreen(
     modifier: Modifier = Modifier,
     onItemClick: (id: Int) -> Unit
 ) {
-
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.onPrimary
+    ) {
+        when(uiState) {
+            UserContract.State.Fail -> Unit
+            is UserContract.State.FailWithException -> Unit
+            UserContract.State.Loading -> Unit
+            is UserContract.State.Success -> {
+                UserItems(userItems = uiState.users)
+            }
+        }
+    }
 }
 
 @Composable
